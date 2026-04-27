ccbb3ebe482a2b5a44ff3589c89a9af42349107e
Tomasz Slimak <remzak.k@gmail.com>
2026-04-27 23:03:37 +0200

    feaT: sex

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,16 +3,22 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { alphaTab } from "@coderline/alphatab-vite";
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    alphaTab(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+
+  optimizeDeps: {
+    exclude: ['@coderline/alphatab']
+  }
 })
