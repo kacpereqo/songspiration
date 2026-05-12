1f23a6b3bfd83b2749aad428ef705527992470b0
AleksandraWronaa <awrona.biznes@gmail.com>
2026-05-13 01:35:29 +0200

    Add business logic classes and unit tests for admin panel functionality

--- backend/SongSpiration.BLL/Interfaces/IAdminPanelService.cs
+++ backend/SongSpiration.BLL/Interfaces/IAdminPanelService.cs
@@ -1,19 +1,22 @@
+using SongSpiration.BLL.DTOs;
 using SongSpiration.Models;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace SongSpiration.BLL.Interfaces
 {
     public interface IAdminPanelService
     {
+        Task<User> GetUserByIdAsync(int userId);
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> CreateUserAsync(UserCreateDto userCreateDto);
+        Task UpdateUserAsync(int userId, UserUpdateDto userUpdateDto);
+        Task DeleteUserAsync(int userId);
         Task<List<Genre>> GetAllGenresAsync();
-        Task<Genre> GetGenreByIdAsync(int id);
+        Task<Genre> GetGenreByIdAsync(int genreId);
         Task<Genre> CreateGenreAsync(GenreCreateDto genreCreateDto);
-        Task<Genre> UpdateGenreAsync(int id, GenreCreateDto genreCreateDto);
-        Task DeleteGenreAsync(int id);
-        Task<List<User>> SearchUsersAsync(string criteria);
-        Task DeleteUserAsync(int id);
-        Task BanUserAsync(int id);
-        Task DeletePinsForUserAsync(int id);
+        Task UpdateGenreAsync(int genreId, GenreCreateDto genreCreateDto);
+        Task DeleteGenreAsync(int genreId);
+        Task<List<User>> SearchUsersAsync(string searchTerm);
+        Task BanUserAsync(int userId);
+        Task DeletePinsForUserAsync(int userId);
     }
 }
--- backend/SongSpiration.BLL/Services/AdminPanelService.cs
+++ backend/SongSpiration.BLL/Services/AdminPanelService.cs
@@ -1,64 +1,154 @@
 using SongSpiration.BLL.Interfaces;
+using SongSpiration.DAL.Interfaces;
 using SongSpiration.Models;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+using SongSpiration.BLL.DTOs;
 
 namespace SongSpiration.BLL.Services
 {
-    public class AdminPanelService
+    public class AdminPanelService : IAdminPanelService
     {
-        private readonly IGenreService _genreService;
-        private readonly IUserService _userService;
+        private readonly IUserRepository _userRepository;
+        private readonly IGenreRepository _genreRepository;
+        private readonly IPinRepository _pinRepository;
 
-        public AdminPanelService(IGenreService genreService, IUserService userService)
+        public AdminPanelService(IUserRepository userRepository, IGenreRepository genreRepository, IPinRepository pinRepository)
         {
-            _genreService = genreService;
-            _userService = userService;
+            _userRepository = userRepository;
+            _genreRepository = genreRepository;
+            _pinRepository = pinRepository;
+        }
+
+        public async Task<User> GetUserByIdAsync(int userId)
+        {
+            return await _userRepository.GetByIdAsync(userId);
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            return await _userRepository.GetAllAsync();
+        }
+
+        public async Task<User> CreateUserAsync(UserCreateDto userCreateDto)
+        {
+            var user = new User
+            {
+                Username = userCreateDto.Username,
+                Email = userCreateDto.Email,
+                PasswordHash = userCreateDto.PasswordHash,
+                Role = userCreateDto.Role
+            };
+
+            await _userRepository.AddAsync(user);
+            await _userRepository.SaveAsync();
+
+            return user;
+        }
+
+        public async Task UpdateUserAsync(int userId, UserUpdateDto userUpdateDto)
+        {
+            var user = await _userRepository.GetByIdAsync(userId);
+            if (user == null)
+            {
+                throw new Exception("User not found");
+            }
+
+            user.Username = userUpdateDto.Username;
+            user.Email = userUpdateDto.Email;
+            user.PasswordHash = userUpdateDto.PasswordHash;
+            user.Role = userUpdateDto.Role;
+
+            _userRepository.Update(user);
+            await _userRepository.SaveAsync();
+        }
+
+        public async Task DeleteUserAsync(int userId)
+        {
+            var user = await _userRepository.GetByIdAsync(userId);
+            if (user == null)
+            {
+                throw new Exception("User not found");
+            }
+
+            _userRepository.Delete(user);
+            await _userRepository.SaveAsync();
         }
 
         public async Task<List<Genre>> GetAllGenresAsync()
         {
-            return await _genreService.GetAllGenresAsync();
+            return await _genreRepository.GetAllAsync();
         }
 
-        public async Task<Genre> GetGenreByIdAsync(int id)
+        public async Task<Genre> GetGenreByIdAsync(int genreId)
         {
-            return await _genreService.GetGenreByIdAsync(id);
+            return await _genreRepository.GetByIdAsync(genreId);
         }
 
         public async Task<Genre> CreateGenreAsync(GenreCreateDto genreCreateDto)
         {
-            return await _genreService.CreateGenreAsync(genreCreateDto);
+            var genre = new Genre
+            {
+                Name = genreCreateDto.Name
+            };
+
+            await _genreRepository.AddAsync(genre);
+            await _genreRepository.SaveAsync();
+
+            return genre;
         }
 
-        public async Task<Genre> UpdateGenreAsync(int id, GenreCreateDto genreCreateDto)
+        public async Task UpdateGenreAsync(int genreId, GenreCreateDto genreCreateDto)
         {
-            return await _genreService.UpdateGenreAsync(id, genreCreateDto);
+            var genre = await _genreRepository.GetByIdAsync(genreId);
+            if (genre == null)
+            {
+                throw new Exception("Genre not found");
+            }
+
+            genre.Name = genreCreateDto.Name;
+
+            _genreRepository.Update(genre);
+            await _genreRepository.SaveAsync();
         }
 
-        public async Task DeleteGenreAsync(int id)
+        public async Task DeleteGenreAsync(int genreId)
         {
-            await _genreService.DeleteGenreAsync(id);
+            var genre = await _genreRepository.GetByIdAsync(genreId);
+            if (genre == null)
+            {
+                throw new Exception("Genre not found");
+            }
+
+            _genreRepository.Delete(genre);
+            await _genreRepository.SaveAsync();
         }
 
-        public async Task<List<User>> SearchUsersAsync(string criteria)
+        public async Task<List<User>> SearchUsersAsync(string searchTerm)
         {
-            return await _userService.SearchUsersAsync(criteria);
+            return await _userRepository.SearchAsync(searchTerm);
         }
 
-        public async Task DeleteUserAsync(int id)
+        public async Task BanUserAsync(int userId)
         {
-            await _userService.DeleteUserAsync(id);
+            var user = await _userRepository.GetByIdAsync(userId);
+            if (user == null)
+            {
+                throw new Exception("User not found");
+            }
+
+            user.IsBanned = true;
+
+            _userRepository.Update(user);
+            await _userRepository.SaveAsync();
         }
 
-        public async Task BanUserAsync(int id)
+        public async Task DeletePinsForUserAsync(int userId)
         {
-            await _userService.BanUserAsync(id);
-        }
-
-        public async Task DeletePinsForUserAsync(int id)
-        {
-            await _userService.DeletePinsForUserAsync(id);
+            var pins = await _pinRepository.GetPinsByUserIdAsync(userId);
+            foreach (var pin in pins)
+            {
+                _pinRepository.Delete(pin);
+            }
+            await _pinRepository.SaveAsync();
         }
     }
 }
